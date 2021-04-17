db060b674b7201b0e65d9ae1395d226519f3df49
Thomas <thomas@craftacademy.se>
2021-04-17 11:41:18 +0200

    adds a new way of setting subject and working with objects in tests

--- basic_unit_testing/specs/person.spec.js
+++ basic_unit_testing/specs/person.spec.js
@@ -3,52 +3,65 @@ const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const Person = require('../src/person')
 use(sinonChai)
+global.expect = expect;
 
-const subject = new Person({ firstName: 'Jane', lastName: 'Doe', age: 25 })
+
 
 describe('instance of Person', () => {
-  it('is expected to be an instance of Person class', () => {
-    expect(subject).to.be.instanceOf(Person)
-  });
+  subject(() => new Person($options))
 
-  it('is expected to have property firstName', () => {
-    expect(subject)
-      .to.haveOwnProperty('firstName')
-      .and.equal('Jane')
-  });
+  describe('properties setters on instantiation', () => {
 
-  it('is expected to have property lastName', () => {
-    expect(subject)
-      .to.haveOwnProperty('lastName')
-      .and.equal('Doe')
-  });
+    def('options', () => ({ firstName: 'Jane', lastName: 'Doe', age: 25 }))
+    it('is expected to be an instance of Person class', () => {
+      expect($subject).to.be.instanceOf(Person)
+    });
 
-  it('is expected to have property age', () => {
-    expect(subject)
-      .to.haveOwnProperty('age')
-      .and.equal(25)
+    it(() => is.expected.to.haveOwnProperty('firstName').which.equal('Jane'))
+    its('firstName', () => is.expected.to.equal('Jane'))
+
+
+    it('is expected to have property firstName', () => {
+      expect($subject)
+        .to.haveOwnProperty('firstName')
+        .and.equal('Jane')
+    });
+
+    it('is expected to have property lastName', () => {
+      expect($subject)
+        .to.haveOwnProperty('lastName')
+        .and.equal('Doe')
+    });
+
+    it('is expected to have property age', () => {
+      expect($subject)
+        .to.haveOwnProperty('age')
+        .and.equal(25)
+    });
   });
 
   describe('property setters', () => {
+    def('options', () => ({}))
+
     describe('setFirstName', () => {
       it('is expected to respond to setFirstName', () => {
-        expect(subject).to.respondTo('setFirstName')
+        expect($subject).to.respondTo('setFirstName')
       });
 
       it('is expected to change the firstName property', () => {
-        subject.setFirstName('Mike')
-        expect(subject.firstName).to.equal('Mike')
+        $subject.setFirstName('Mike')
+        expect($subject.firstName).to.equal('Mike')
       });
     });
 
     describe('setLastName', () => {
       it('is expected to respond to setLastName', () => {
-        expect(subject).to.respondTo('setLastName')
+        expect($subject).to.respondTo('setLastName')
       });
 
       it('is expected to change the firstName property', () => {
-        subject.setLastName('Ochman')
-        expect(subject.lastName()).to.equal('Ochman')
+        $subject.setLastName('Ochman')
+        expect($subject.lastName).to.equal('Ochman')
       });
 
     });
@@ -56,25 +69,28 @@ describe('instance of Person', () => {
 
   describe('dynamic getters', () => {
     describe('fullName', () => {
+      def('options', () => ({}))
+
       it('is expected to respond to fullName', () => {
-        expect(subject).to.respondTo('fullName')
+        expect($subject).to.respondTo('fullName')
       });
 
       it('is expected to combine firstName with lastName', () => {
-        subject.firstName = "Thomas"
-        subject.lastName = "Ochman"
-        expect(subject.fullName()).to.equal('Thomas Ochman')
+        $subject.firstName = "Thomas"
+        $subject.lastName = "Ochman"
+        expect($subject.fullName()).to.equal('Thomas Ochman')
       });
     });
+    describe('fullDetails', () => {
+      def('options', () => ({ firstName: 'Jane', lastName: 'Doe', age: 25 }))
 
-    describe.only('fullDetails', () => {
 
       let fullNameSpy, response
 
       before(() => {
         // 1. set up an observable on the fullName function
-        fullNameSpy = sinon.spy(subject, 'fullName')
-        response = subject.fullDetails()
+        fullNameSpy = sinon.spy($subject, 'fullName')
+        response = $subject.fullDetails()
       });
 
       it('is expected to call on fullName', () => {
