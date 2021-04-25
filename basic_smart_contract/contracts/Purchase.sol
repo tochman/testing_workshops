fa4bb079544231953ed61cad19a9e41c94097916
Thomas <thomas@craftacademy.se>
2021-04-25 12:30:55 +0200

    WIP adds restriction for buyer to initiate purchase process

--- basic_smart_contract/contracts/Purchase.sol
+++ basic_smart_contract/contracts/Purchase.sol
@@ -1,22 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
 
 contract Purchase {
-  address public buyerAddress;
-  address public serviceProviderAddress;
-  string public status;
+    address public buyerAddress;
+    address public serviceProviderAddress;
+    string public status;
+    struct RequiredService {
+        string service;
+        uint256 amount;
+        string unit;
+    }
 
-  constructor(address _buyerAddress) {
-    buyerAddress = _buyerAddress;
-    serviceProviderAddress = msg.sender;
-  }
+    RequiredService public requiredService;
 
-  function initiate() public returns (string memory){
-    status = 'initiated';
-    return status; 
-  }
+    constructor(address _buyerAddress) {
+        buyerAddress = _buyerAddress;
+        serviceProviderAddress = msg.sender;
+        status = "deployed";
+    }
 
+    function initiate( string memory service, uint256 amount, string memory unit) external {
+      // 1. check if the signer is the buyer
+      if (msg.sender == buyerAddress) {
+      // 3. if YES: Then move on and set the req Service
+        // console.log(msg.sender);
+        status = "initiated";
+        requiredService = RequiredService(service, amount, unit);
+
+      } else {
+      // 2. if NOT: Get the hell out of here
+
+      }
+    }
 }
-
--- basic_smart_contract/test/Purchase.test.js
+++ basic_smart_contract/test/Purchase.test.js
@@ -4,7 +4,7 @@ const Purchase = require('../src/artifacts/contracts/Purchase.sol/Purchase.json'
 
 
 describe('Purchase', async () => {
-  const [serviceProvider, buyer] = waffle.provider.getWallets()
+  const [serviceProvider, buyer, randomWallet] = waffle.provider.getWallets()
 
   beforeEach(async () => {
     contract = await waffle.deployContract(serviceProvider, Purchase, [buyer.address])
@@ -22,22 +22,53 @@ describe('Purchase', async () => {
     expect(await contract.buyerAddress()).to.eql(buyer.address)
   });
 
-  describe('buyer can initiate the purchase process', () => {
+  describe('Buyer can initiate the purchase process', () => {
     
     beforeEach(async () => {
-      contract.initiate()
-
-      // {service: 'Software Development', amount: 10, unit: 'h'}
+      // contract.connect(buyer)
+      // await buyer.signMessage(contract.initiate('Software Development', 10, 'h'))
+      // await contract.initiate('Software Development', 10, 'h')
+      await contract.connect(buyer).initiate('Software Development', 10, 'h')
+      requiredService = await contract.requiredService()
+      expectedrequiredService = {service: 'Software Development', amount: 10, unit: 'h'}
     });
 
-    it.only('is expected to set contract status to "initiated"', async () => {
+    it('is expected to set contract status to "initiated"', async () => {
       expect(await contract.status()).to.eql('initiated')
     });
 
-    it('is expected to set contract requiredService to "Software Development"', () => {
-      
+    it('is expected to set contract requiredService service to "Software Development"', async () => {
+      expect(requiredService[0]).to.eql(expectedrequiredService.service)
+    });
+
+    it('is expected to set contract requiredService amount to 10', async () => {
+      expect(parseInt(requiredService[1])).to.eql(expectedrequiredService.amount)
+    });
+
+    it('is expected to set contract requiredService unit to "h"', async () => {
+      expect(requiredService[2]).to.eql(expectedrequiredService.unit)
     });
 
   });
 
+  describe('Random Wallet can NOT initiate the purchase process', () => {
+    
+    beforeEach(async () => {
+      // contract.connect(buyer)
+      // await buyer.signMessage(contract.initiate('Software Development', 10, 'h'))
+      // await contract.initiate('Software Development', 10, 'h')
+      await contract.connect(randomWallet).initiate('Software Development', 10, 'h')
+      requiredService = await contract.requiredService()
+      expectedrequiredService = {service: 'Software Development', amount: 10, unit: 'h'}
+    });
+
+    it('is expected to set contract status to "initiated"', async () => {
+      expect(await contract.status()).to.eql('deployed')
+    });
+
+    // it('is expected to set contract requiredService service to "Software Development"', async () => {
+    //   expect(requiredService[0]).to.eql(null)
+    // });
+
+  });
 });
