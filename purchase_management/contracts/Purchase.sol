e410e5de9f29ec2109e70a06783246f0b135e005
Thomas <thomas@craftacademy.se>
2021-05-02 09:01:22 +0200

    creates bacis react app structure includes Pusrchase contract and hardhat-waffle test

---
+++ purchase_management/contracts/Purchase.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "hardhat/console.sol";
+
+contract Purchase {
+    address public buyerAddress;
+    address public serviceProviderAddress;
+    struct RequiredService {
+        string service;
+        uint32 amount;
+        string unit;
+    }
+
+    enum ProcessStatus {
+        Deployed,
+        Initiated,
+        InProgress,
+        Delivered,
+        Accepted,
+        Finalized
+    }
+
+    event Initialize(
+        address indexed from,
+        string service,
+        uint32 amount,
+        string unit
+    );
+
+    RequiredService public requiredService;
+    ProcessStatus public status = ProcessStatus.Delivered;
+
+    constructor(address _buyerAddress) {
+        buyerAddress = _buyerAddress;
+        serviceProviderAddress = msg.sender;
+    }
+
+    function initiate(
+        string memory service,
+        uint32 amount,
+        string memory unit
+    ) external {
+        // require(msg.sender != serviceProviderAddress, "It does not make sense to perform that operation!");
+        require(msg.sender == buyerAddress, msg.sender != serviceProviderAddress ?  "You can't perform this operation" : "It does not make sense to perform that operation!");
+        status = ProcessStatus.Initiated;
+        requiredService = RequiredService(service, amount, unit);
+        emit Initialize(msg.sender, service, amount, unit);
+    }
+}
---
+++ purchase_management/hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    }
+  }
+};
