3b96fbc16cf4bae1f74bfb70b85395897f28f32f
Thomas <thomas@craftacademy.se>
2021-05-09 11:13:59 +0200

    WIP adds deploy contract functionality still issues with gas proce and connecting to the right testing chain (localhost)

--- purchase_management/src/App.jsx
+++ purchase_management/src/App.jsx
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import detectEthereumProvider from '@metamask/detect-provider'
+import { ethers } from "ethers";
 
-// import { ethers } from 'ethers'
+import detectEthereumProvider from '@metamask/detect-provider'
+import DeployConstract from "./components/DeployContract";
 
 // 1. Make sure that the app knows who I am
 // 2. Build an interface to input the Buyers address and get the info needed to deploy the contract
-// 3. Deploy the Purchase contract
+// 3. Deploy the Purchase contract (validate the address beforehand?)
 
 const App = () => {
-  const [address, setAddress ] = useState("Loading...")
+  const [address, setAddress] = useState("Loading...")
+  const [w3provider, setW3Provider] = useState({})
 
   const requestAccount = async () => {
     const provider = await detectEthereumProvider();
     if (provider) {
-      return provider.selectedAddress
+      // A Web3Provider wraps a standard Web3 provider, which is
+      // what Metamask injects as window.ethereum into each page
+      const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
+    
+      return web3Provider
     } else {
       return "No address can be foiund, please install MetaMask"
     }
   }
 
   useEffect(() => {
-    requestAccount().then(address => setAddress(address))
+    requestAccount().then(provider => {
+      setW3Provider(provider)
+    })
   }, [])
   return (
     <>
       <h1>Purchase Contract</h1>
-      <p>{address}</p>
+      <DeployConstract provider={w3provider} />
     </>
   )
 }
