574c09fec62e31f2a43679507d27b2255e6d324e
Thomas <thomas@craftacademy.se>
2021-06-05 13:37:01 +0200

    adds sample puppeteer script refactors tests solves the transaction count myster

--- purchase_management/src/App.jsx
+++ purchase_management/src/App.jsx
@@ -1,31 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { MDBContainer } from "mdbreact";
-import detectEthereumProvider from '@metamask/detect-provider'
+import detectEthereumProvider from "@metamask/detect-provider";
 import DeployContract from "./components/DeployContract";
 import ViewContract from "./components/ViewContract";
 
 const App = () => {
-  const [address, setAddress] = useState("Loading...")
-  const [w3provider, setW3Provider] = useState({})
-  
+  const [address, setAddress] = useState("Loading...");
+  const [w3provider, setW3Provider] = useState({});
+
   const requestAccount = async () => {
     const provider = await detectEthereumProvider();
     if (provider) {
-      !provider.isConnected() &&  await window.ethereum.request('eth_requestAccounts')
-      const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
-      return web3Provider
+      !provider.isConnected() && (await window.ethereum.enable());
+
+      // !provider.isConnected() &&  await window.ethereum.request('eth_requestAccounts')
+      const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+      return web3Provider;
     } else {
-      return "No address can be found, please install MetaMask"
+      return "No address can be found, please install MetaMask";
     }
-  }
-  
+  };
+
   useEffect(() => {
-    requestAccount().then(provider => {
-      setAddress(window.ethereum.selectedAddress)
-      setW3Provider(provider)
-    })
-  }, [])
+    requestAccount().then((provider) => {
+      setAddress(window.ethereum.selectedAddress);
+      setW3Provider(provider);
+    });
+  }, []);
 
   return (
     <MDBContainer>
@@ -34,7 +36,7 @@ const App = () => {
       <DeployContract provider={w3provider} />
       <ViewContract provider={w3provider} />
     </MDBContainer>
-  )
-}
+  );
+};
 
-export default App
+export default App;
